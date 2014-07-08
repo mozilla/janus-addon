d094b055748c6f705ef8ed91c26ce1ee7f111d8e
James Willcox <snorp@snorp.net>
2014-07-08 14:04:18 -0500

    Gonzales was renamed to Janus, change things accordingly

--- bootstrap.js
+++ bootstrap.js
@@ -6,21 +6,21 @@ Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Preferences.jsm");
 
 const NS_XUL = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-const ADDON_ID = "gonzales@mozilla.org";
+const ADDON_ID = "janus@mozilla.org";
 
-const GONZALES_ENABLED_PREF = "extensions.gonzales.enabled";
-const GONZALES_PAC_URL_PREF = "extensions.gonzales.pac_url";
+const JANUS_ENABLED_PREF = "extensions.janus.enabled";
+const JANUS_PAC_URL_PREF = "extensions.janus.pac_url";
 
-const GONZALES_ADBLOCK_ENABLED_PREF = "extensions.gonzales.adblock.enabled";
-const GONZALES_GIF2VIDEO_ENABLED_PREF = "extensions.gonzales.gif2video.enabled";
+const JANUS_ADBLOCK_ENABLED_PREF = "extensions.janus.adblock.enabled";
+const JANUS_GIF2VIDEO_ENABLED_PREF = "extensions.janus.gif2video.enabled";
 
 const PROXY_AUTOCONFIG_URL_PREF = "network.proxy.autoconfig_url";
 const PROXY_TYPE_PREF = "network.proxy.type";
 
-const DEFAULT_PROXY_URL = "http://gonzales.allizom.org";
+const DEFAULT_PROXY_URL = "http://janus.allizom.org";
 const PROXY_TYPE = 2;
 
-const GONZALES_MENU_ID = "tools-gonzales-toggle";
+const JANUS_MENU_ID = "tools-janus-toggle";
 
 function isNativeUI() {
   return (Services.appinfo.ID == "{aa3c5121-dab2-40e2-81ca-7ea25febc110}");
@@ -28,7 +28,7 @@ function isNativeUI() {
 
 var gAndroidMenuId = null;
 
-function setGonzalesChecked(checked) {
+function setProxyChecked(checked) {
   let windows = Services.wm.getEnumerator("navigator:browser");
   while (windows.hasMoreElements()) {
     let win = windows.getNext();
@@ -38,24 +38,22 @@ function setGonzalesChecked(checked) {
         checked: checked
       });
     } else {
-      let menuItem = win.document.getElementById(GONZALES_MENU_ID);
-
-      console.log("setting menuitem checked: " + checked);
+      let menuItem = win.document.getElementById(JANUS_MENU_ID);
       menuItem.setAttribute("checked", checked);
     }
   }
 }
 
-var GonzalesAddon = {
+var ProxyAddon = {
 
   rebuildHeader: function() {
     this.header = "";
 
-    if (Preferences.get(GONZALES_ADBLOCK_ENABLED_PREF, false)) {
+    if (Preferences.get(JANUS_ADBLOCK_ENABLED_PREF, false)) {
       this.header += "+adblock ";
     }
 
-    if (Preferences.get(GONZALES_GIF2VIDEO_ENABLED_PREF, false)) {
+    if (Preferences.get(JANUS_GIF2VIDEO_ENABLED_PREF, false)) {
       this.header += "+gif2video ";
     }
   },
@@ -63,54 +61,50 @@ var GonzalesAddon = {
   applyPrefChanges: function(name) {
     var value = Preferences.get(name);
 
-    if (name === GONZALES_ENABLED_PREF) {
-      console.log("setting gonzales enabled: " + value);
+    if (name === JANUS_ENABLED_PREF) {
 
-      setGonzalesChecked(value);
+      setProxyChecked(value);
       this.enabled = value;
 
       if (value) {
-        Preferences.set(PROXY_AUTOCONFIG_URL_PREF, Preferences.get(GONZALES_PAC_URL_PREF));
+        Preferences.set(PROXY_AUTOCONFIG_URL_PREF, Preferences.get(JANUS_PAC_URL_PREF));
         Preferences.set(PROXY_TYPE_PREF, PROXY_TYPE);
-        Services.obs.addObserver(GonzalesAddon.observe, "http-on-modify-request", false);
+        Services.obs.addObserver(ProxyAddon.observe, "http-on-modify-request", false);
       } else {
         Preferences.reset(PROXY_AUTOCONFIG_URL_PREF);
         Preferences.reset(PROXY_TYPE_PREF);
 
         try {
-          Services.obs.removeObserver(GonzalesAddon.observe, "http-on-modify-request");
+          Services.obs.removeObserver(ProxyAddon.observe, "http-on-modify-request");
         } catch(e) {}
       }
 
       this.rebuildHeader();
-    } else if (name === GONZALES_PAC_URL_PREF) {
+    } else if (name === JANUS_PAC_URL_PREF) {
       Preferences.set(PROXY_AUTOCONFIG_URL_PREF, value);
-    } else if (name === GONZALES_ADBLOCK_ENABLED_PREF ||
-               name === GONZALES_GIF2VIDEO_ENABLED_PREF) {
+    } else if (name === JANUS_ADBLOCK_ENABLED_PREF ||
+               name === JANUS_GIF2VIDEO_ENABLED_PREF) {
       this.rebuildHeader();
     }
   },
 
   observe: function(subject, topic, data) {
-    console.log("topic: " + topic);
     if (topic === "nsPref:changed") {
       this.applyPrefChanges(data);
     } else if (topic === "http-on-modify-request") {
       let channel = subject.QueryInterface(Ci.nsIHttpChannel);
 
-      console.log("X-Gonzales-Options: " + GonzalesAddon.header);
-      channel.setRequestHeader("X-Gonzales-Options", GonzalesAddon.header, false);
+      channel.setRequestHeader("X-Janus-Options", ProxyAddon.header, false);
     }
   },
 }
 
 var gWindows = [];
 
-function onUseGonzalesClick() {
-  console.log("toggling gonzales");
-  var enabled = Preferences.get(GONZALES_ENABLED_PREF);
+function onUseProxyClick() {
+  var enabled = Preferences.get(JANUS_ENABLED_PREF);
   enabled = !enabled;
-  Preferences.set(GONZALES_ENABLED_PREF, enabled);
+  Preferences.set(JANUS_ENABLED_PREF, enabled);
 }
 
 function loadIntoWindow(window) {
@@ -119,19 +113,19 @@ function loadIntoWindow(window) {
 
   if (isNativeUI()) {
     gAndroidMenuId = window.NativeWindow.menu.add({
-      name: 'Use Gonzales',
+      name: 'Use Proxy',
       checkable: true,
-      checked: Preferences.get(GONZALES_ENABLED_PREF),
+      checked: Preferences.get(JANUS_ENABLED_PREF),
       parent: window.NativeWindow.menu.toolsMenuID,
-      callback: onUseGonzalesClick
+      callback: onUseProxyClick
     });
   } else {
     let menuItem = window.document.createElementNS(NS_XUL, "menuitem");
-    menuItem.setAttribute("id", GONZALES_MENU_ID);
-    menuItem.setAttribute("label", "Use Gonzales");
-    menuItem.setAttribute("checked", Preferences.get(GONZALES_ENABLED_PREF));
+    menuItem.setAttribute("id", JANUS_MENU_ID);
+    menuItem.setAttribute("label", "Use Proxy");
+    menuItem.setAttribute("checked", Preferences.get(JANUS_ENABLED_PREF));
     menuItem.setAttribute("class", "menuitem-iconic");
-    menuItem.addEventListener("command", onUseGonzalesClick);
+    menuItem.addEventListener("command", onUseProxyClick);
     window.document.getElementById("menu_ToolsPopup").appendChild(menuItem);
   }
 
@@ -146,7 +140,7 @@ function unloadFromWindow(window) {
     window.NativeWindow.menu.remove(gAndroidMenuId);
     gAndroidMenuId = null;
   } else {
-    window.document.getElementById(GONZALES_MENU_ID).remove();
+    window.document.getElementById(JANUS_MENU_ID).remove();
   }
 }
 
@@ -171,18 +165,18 @@ var windowListener = {
   }
 };
 
-const OBSERVE_PREFS = [GONZALES_ENABLED_PREF, GONZALES_PAC_URL_PREF,
-                       GONZALES_ADBLOCK_ENABLED_PREF, GONZALES_GIF2VIDEO_ENABLED_PREF];
+const OBSERVE_PREFS = [JANUS_ENABLED_PREF, JANUS_PAC_URL_PREF,
+                       JANUS_ADBLOCK_ENABLED_PREF, JANUS_GIF2VIDEO_ENABLED_PREF];
 
 function startup(aData, aReason) {
 
   if (aReason === ADDON_INSTALL) {
-    Preferences.set(GONZALES_ENABLED_PREF, false);
-    Preferences.set(GONZALES_PAC_URL_PREF, DEFAULT_PROXY_URL);
+    Preferences.set(JANUS_ENABLED_PREF, false);
+    Preferences.set(JANUS_PAC_URL_PREF, DEFAULT_PROXY_URL);
   }
 
   OBSERVE_PREFS.forEach(function(pref) {
-    Preferences.observe(pref, GonzalesAddon);
+    Preferences.observe(pref, ProxyAddon);
   });
 
   // Load into any existing windows
@@ -195,7 +189,7 @@ function startup(aData, aReason) {
   // Load into any new windows
   Services.wm.addListener(windowListener);
 
-  GonzalesAddon.applyPrefChanges(GONZALES_ENABLED_PREF);
+  ProxyAddon.applyPrefChanges(JANUS_ENABLED_PREF);
 }
 
 function shutdown(aData, aReason) {
@@ -203,7 +197,7 @@ function shutdown(aData, aReason) {
     return;
 
   OBSERVE_PREFS.forEach(function(pref) {
-    Preferences.ignore(pref, GonzalesAddon);
+    Preferences.ignore(pref, ProxyAddon);
   });
 
   // Put proxy prefs back to defaults
